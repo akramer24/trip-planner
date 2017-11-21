b5c76026f5b84e5c0416acf4e48e494219e5d14d
Ari Kramer <arikramer24@gmail.com>
2017-11-21 12:01:21 -0500

    added marker.js

---
+++ src/marker.js
@@ -0,0 +1,19 @@
+// const markerDomEl = document.createElement("div");
+// markerDomEl.style.width = '32px';
+// markerDomEl.style.height = '39px';
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+const hotelMarker = document.createElement('div');
+hotelMarker.style.width = '32px';
+hotelMarker.style.height = '39px';
+hotelMarker.style.backgroundImage = "url(http://i.imgur.com/D9574Cu.png)";
+
+const restMarker = document.createElement('div');
+resMarker.style.width = '32px';
+resMarker.style.height = '39px';
+resMarker.style.backgroundImage = "url(http://i.imgur.com/cqR6pUI.png)";
+
+const activityMarker = document.createElement('div');
+activityMarker.style.width = '32px';
+activityMarker.style.height= '39px';
+activityMarker.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)"
